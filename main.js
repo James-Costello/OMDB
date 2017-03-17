e6e5c41aaa0c2cb3f90b5375081327224a95e78d
James Costello <jcostello10@gmail.com>
2017-03-17 13:00:09 -0700

    api functions

---
+++ main.js
@@ -0,0 +1,30 @@
+$(document).ready(() => {
+  $('#searchForm').on('submit', (e) => {
+    let searchText = $('#searchText').val();
+    getMovies(searchText);
+    e.preventDefault();
+  })
+});
+
+function getMovies(searchText){
+  axios.get('http://www.omdbapi.com?s='+searchText)
+    .then((response) => {
+      console.log(response);
+      let movies = response.data.Search;
+      let output = '';
+      $.each(movies, (index, movie) => { //Passing in movie's array
+        output += `
+          <div class="col-md-3">
+            <div class="well text-center">
+              <img src="${movie.Poster}">
+              <h5>${movie.Title}</h5>
+              <a onclick="movieSelected('$(movie.imdbID)') "
+            </div>
+          </div>
+        `
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
